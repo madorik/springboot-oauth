2f18533c33e91233fb09505ae46cfc6ea454b00a
madorik <xornjs1988@gmail.com>
2022-02-17 16:11:29 +0900

    init

---
+++ src/test/java/com/madorik/springbootoauth/SpringbootOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madorik.springbootoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
